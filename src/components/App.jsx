d645edc37c9d41294f8c4765eedf49e3e96a8d4a
Paweł <102540215+przytop@users.noreply.github.com>
2024-09-10 01:12:15 +0200

    Update App.jsx
    updated contacts state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,17 @@ import ContactList from "./ContactList";
 import { FaAddressBook } from "react-icons/fa";
 
 export default function App() {
-  const savedContacts = window.localStorage.getItem("saved-contacts");
-
-  const initialContacts = savedContacts
-    ? JSON.parse(savedContacts)
-    : [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ];
-
-  const [contacts, setContacts] = useState(initialContacts);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("saved-contacts");
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
 
   useEffect(() => {
     window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
